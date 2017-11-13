9b1b1e6f66b53b10950af0020d60824e943724c7
Rafael Izidoro <izidoro.rafa@gmail.com>
2017-11-13 19:58:43 -0200

    change version and fix specs

--- lib/kinabo/version.rb
+++ lib/kinabo/version.rb
@@ -1,3 +1,3 @@
 module Kinabo
-  VERSION = "0.4.0"
+  VERSION = "0.5.0"
 end
--- spec/fromtis/remessa/transacao_spec.rb
+++ spec/fromtis/remessa/transacao_spec.rb
@@ -7,7 +7,7 @@ describe Kinabo::Fromtis::Remessa::Transacao do
   let(:transacao_hash) { Fromtis::RemessaMock.transacao }
 
   describe '#build' do
-    let(:line) { '1                   02000000110000  0000000023660999007       0000000000000000000000000348751 190717        77002366099905071700000000334262370000001 010116000020000000000000000000000000000000000000003342600000000000000100030691232288Rogerio Oliveira Santo                                                          00000000000000000000RUBYBANK                                      59156195000106                                            ' }
+    let(:line) { '1                   0200    110000  0000000023660999007       0000000000000000000000000348751 190717        77002366099905071700000000334262370000001 010116000020000000000000000000000000000000000000003342600000000000000100030691232288Rogerio Oliveira Santo                                                          00000000000000000000RUBYBANK                                      59156195000106                                            ' }
 
     it 'builds the "transacao" part' do
       expect(transacao.build).to eq(line)
